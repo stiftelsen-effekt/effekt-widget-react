30300f1994f3f472dd81338eeaa845392af75c64
Philip <philip.h.andersen@gmail.com>
2021-02-25 20:10:55 +0100

    Fixes env api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,18 +1,9 @@
 let apiUrl;
 
-switch (process.env.NODE_ENV) {
-  case "development":
-    apiUrl = "https://dev.data.gieffektivt.no";
-    break;
-  case "test":
-    apiUrl = "https://stage.data.gieffektivt.no";
-    break;
-  case "production":
-    apiUrl = "https://data.gieffektivt.no";
-    break;
-  default:
-    apiUrl = "https://dev.data.gieffektivt.no";
-    break;
+if (process.env.REACT_APP_EFFEKT_API_URL) {
+  apiUrl = process.env.REACT_APP_EFFEKT_API_URL;
+} else {
+  apiUrl = "https://dev.data.gieffektivt.no";
 }
 
 export const API_URL = apiUrl;
