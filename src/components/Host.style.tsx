ab4e6ea8da361be4dd1643728db70aace70c3479
Philip <philip.h.andersen@gmail.com>
2021-02-25 22:24:17 +0100

    Removes splash image

--- src/components/Host.style.tsx
+++ src/components/Host.style.tsx
@@ -6,18 +6,6 @@ export const WidgetWrapper = styled.div`
   justify-content: center;
 `;
 
-export const Splash = styled.div`
-  background-image: url("https://thumb.tildacdn.com/tild3231-3764-4630-b635-313165393338/-/format/webp/SCI2.png");
-  background-size: cover;
-  background-position: center top;
-  height: 235px;
-  width: 100%;
-  display: flex;
-  align-items: flex-end;
-  padding-left: 20px;
-  box-sizing: border-box;
-`;
-
 export const SplashTitle = styled.h1`
   font-family: "Georgia";
   font-weight: 400;
