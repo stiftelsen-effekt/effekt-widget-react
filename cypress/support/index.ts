3efc318afbb9e66dba30f797c16e45b054848a2c
Philip <philip.h.andersen@gmail.com>
2021-02-26 23:38:37 +0100

    TS config for Cypress

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -13,8 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-react-selector';
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
