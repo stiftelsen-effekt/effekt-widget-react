17186b470799bde2ba414973ef61eee5101daec8
Philip <philip.h.andersen@gmail.com>
2021-03-01 18:16:07 +0100

    Fixes bug which rendered PaymentPane at all panenumbers

--- src/components/panes/PaymentPane/PaymentPane.tsx
+++ src/components/panes/PaymentPane/PaymentPane.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { State } from "../../../store/state";
 import { PaymentMethod } from "../../../types/Enums";
+import { Pane } from "../Panes.style";
 import { ResultPane } from "./Bank/ResultPane";
 import { PaypalPane } from "./Paypal/PayPalPane";
 import { VippsPane } from "./Vipps/VippsPane";
@@ -9,14 +10,11 @@ import { VippsPane } from "./Vipps/VippsPane";
 export const PaymentPane: React.FC = () => {
   const method = useSelector((state: State) => state.donation.method);
 
-  switch (method) {
-    case PaymentMethod.BANK:
-      return <ResultPane />;
-    case PaymentMethod.PAYPAL:
-      return <PaypalPane />;
-    case PaymentMethod.VIPPS:
-      return <VippsPane />;
-    default:
-      return <div>Invalid payment method</div>;
-  }
+  return (
+    <Pane>
+      {method === PaymentMethod.BANK && <ResultPane />}
+      {method === PaymentMethod.VIPPS && <VippsPane />}
+      {method === PaymentMethod.PAYPAL && <PaypalPane />}
+    </Pane>
+  );
 };
