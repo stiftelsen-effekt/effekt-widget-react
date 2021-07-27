80fb2ec85504252cbbaabb0b643520d918b05ec7
Philip <44257880+philiphand@users.noreply.github.com>
2021-07-27 11:43:34 +0200

    Removes dueday input

--- src/components/panes/DonationPane/DonationPane.tsx
+++ src/components/panes/DonationPane/DonationPane.tsx
@@ -5,15 +5,10 @@ import {
   registerDonationAction,
   setSum,
   setShareType,
-  setDueDay,
 } from "../../../store/donation/actions";
 import { Pane, PaneContainer } from "../Panes.style";
 import { State } from "../../../store/state";
-import {
-  PaymentMethod,
-  RecurringDonation,
-  ShareType,
-} from "../../../types/Enums";
+import { ShareType } from "../../../types/Enums";
 import { RichSelectOption } from "../../shared/RichSelect/RichSelectOption";
 import { RichSelect } from "../../shared/RichSelect/RichSelect";
 import { NextButton } from "../../shared/Buttons/NavigationButtons.style";
@@ -22,15 +17,11 @@ import { TextInput } from "../../shared/Input/TextInput";
 import { SumWrapper } from "./DonationPane.style";
 import { SharesSum } from "./SharesSum";
 import { LoadingCircle } from "../../shared/LoadingCircle/LoadingCircle";
-import { InfoBox } from "../../shared/Layout/Layout.style";
-import { displayClaimWarning } from "./_util";
 
 export const DonationPane: React.FC = () => {
   const dispatch = useDispatch();
   const donation = useSelector((state: State) => state.donation);
   const [loadingAnimation, setLoadingAnimation] = useState(false);
-
-  const claimWarning = displayClaimWarning(donation);
 
   function onSubmit() {
     if (donation.isValid) {
@@ -70,48 +61,7 @@ export const DonationPane: React.FC = () => {
                   }
                 }}
               />
-
-              {donation.recurring === RecurringDonation.RECURRING &&
-                donation.method === PaymentMethod.BANK && (
-                  <TextInput
-                    label="Trekkdag"
-                    defaultValue={donation.dueDay}
-                    name="dueDay"
-                    type="tel"
-                    clustered
-                    denomination="."
-                    selectOnClick
-                    autoComplete="off"
-                    tooltipText="Vi belaster din konto én gang i måneden på valgte dato."
-                    onChange={(e) => {
-                      if (
-                        Validator.isInt(e.target.value) === true &&
-                        parseInt(e.target.value) < 29 &&
-                        parseInt(e.target.value) > 0
-                      ) {
-                        dispatch(setDueDay(parseInt(e.target.value)));
-                      } else {
-                        dispatch(setDueDay(-1));
-                      }
-                    }}
-                  />
-                )}
             </SumWrapper>
-
-            {donation.dueDay &&
-              (donation.dueDay > 28 || donation.dueDay < 1) && (
-                <InfoBox>
-                  Trekkdato må være mellom den 1. og 28. i en måned.
-                </InfoBox>
-              )}
-
-            {claimWarning && (
-              <InfoBox>
-                Om trekkdatoen settes til under fem dager frem i tid vil vi ikke
-                trekke deg før en måned senere grunnet bankens regler for
-                varslingstid.
-              </InfoBox>
-            )}
 
             <RichSelect
               selected={donation.shareType}
